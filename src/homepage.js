ea31570371e589cd809596df013a4a04ba656cc4
Sumaer Jolly <sumaerjolly@gmail.com>
2019-11-27 19:12:56 +0530

    lint errors fix no 1

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,5 @@
 function homePage() {
   const content = document.querySelector('#content');
-  const home = document.querySelector('#link-0');
   content.innerHTML = '<h1> Jolly Good Burgers </h1>';
   content.classList.add('heading');
   const p = document.createElement('p');
